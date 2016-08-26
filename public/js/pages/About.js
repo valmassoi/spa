1fa49bc0e17ed430aa56189793217e5dcad62d77
valmassoi <rvalmassoi@gmail.com>
2016-08-25 23:25:57 -0700

    init about page

---
+++ public/js/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () =>
+  <div>
+    <h1>About</h1>
+    <p>This is boilerplate to bootsrap a React Single Page App (SPA)</p>
+    <h3>Features</h3>
+    <ul>
+      <li>Pricing tiers</li>
+      <li>Signin page</li>
+    </ul>
+  </div>
+
+export default About
