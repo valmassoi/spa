7891db120132dcd0b177d2fb39b2cdd48b2d7a8f
valmassoi <rvalmassoi@gmail.com>
2016-08-25 23:30:27 -0700

    fix spelling

--- public/js/pages/About.js
+++ public/js/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () =>
   <div>
     <h1>About</h1>
-    <p>This is boilerplate to bootsrap a React Single Page App (SPA)</p>
+    <p>This is boilerplate to bootstrap a React Single Page App (SPA)</p>
     <h3>Features</h3>
     <ul>
       <li>Pricing tiers</li>
