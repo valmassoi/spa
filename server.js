beae44eb65ed7f72300a2e570703ee6ad2604f7d
valmassoi <rvalmassoi@gmail.com>
2016-08-25 17:41:27 -0700

    add node.js server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const http = require('http')
+// const routes = require('./app/routes/someroute')
+// const mongo = require('mongodb').MongoClient
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27018/data'
+
+app.use(express.static(`${__dirname}/public/`))
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.enable('trust proxy')
+
+app.get('*', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('404!')
+})
+
+const port = process.env.PORT || 8081
+
+const server = http.Server(app)
+server.listen(port, () => {
+  console.log(`Server Running on port: ${port}`)
+})
